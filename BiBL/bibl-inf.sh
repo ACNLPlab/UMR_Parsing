1ee6c6a7848066510399177474720ace5cc17211
Emma Markle <130492970+emmamarkle@users.noreply.github.com>
2026-05-12 18:14:13 -0400

    Update bibl-inf.sh

--- BiBL/bibl-inf.sh
+++ BiBL/bibl-inf.sh
@@ -11,22 +11,20 @@ conda activate BIBL
 
 unset PYTHONPATH
 
-#INPUT_FILE="/home/common/ACNLP/umr_parsing/umr_data/test_sentences/gt_eng_test_bibl.txt"
-#INPUT_FILE="/home/common/ACNLP/umr_parsing/data4pipe/data/eng-umr1-FULL.txt"
-INPUT_FILE="/home/common/ACNLP/eng2nav/chrf_bibl_sents.txt"
+INPUT_FILE="bibl_sents.txt"
 
 filename=$(basename "$INPUT_FILE")
 dataset_name=$(echo "$filename")  
 
-OUTPUT_FILE="/home/common/ACNLP/eng2nav/chrf_eng_umrs.txt"
+OUTPUT_FILE="chrf_eng_umrs.txt"
 
-GOLD_FILE="/home/common/ACNLP/umr_parsing/BiBL/gold-file.txt"
+GOLD_FILE="gold-file.txt"
 
-python /home/common/ACNLP/umr_parsing/BiBL/bin/predict_amrs.py \
+python /.../predict_amrs.py \
     --datasets "$INPUT_FILE" \
     --gold-path "$GOLD_FILE" \
     --pred-path "$OUTPUT_FILE" \
-    --checkpoint /home/common/ACNLP/umr_parsing/BiBL/runs/10/best-smatch_checkpoint_2_0.3371.pt \
+    --checkpoint /best_ckpt/best-smatch_checkpoint_2_0.3371.pt \
     --beam-size 3 \
     --batch-size 100 \
     --device cuda \
